8107f5f161e38692e7b7d014268496298b1c9fc6
xiaoixao <1928282732@qq.com>
2020-10-25 16:22:06 +0800

    taobao

---
+++ precache-manifest.91c3b1aa7603913806e30af364f9ab01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93f14517cefb177e4c387c4c4b44bb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "140a05f5228fcc74219d",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "9ee5c72a075649934a50",
+    "url": "./static/css/main.85cc6a76.chunk.css"
+  },
+  {
+    "revision": "140a05f5228fcc74219d",
+    "url": "./static/js/2.bf58d865.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.bf58d865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee5c72a075649934a50",
+    "url": "./static/js/main.c69aa365.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
